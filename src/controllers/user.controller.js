06104da1db340ce91a34e3e6ea5cd65823ea722d
Varun <varun@example.com>
2024-12-21 00:53:03 +0530

    add roters

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@
     import { uploadOnCloudinary } from "../utils/cloudinary.js";
     import {ApiResponse} from "../utils/ApiResponse.js";
 import { Subscription } from "../models/subscription.model.js";
+import mongoose from "mongoose";
 
     const generateAccessAndRefreshToken=async(userId)=>{
         try {
@@ -377,6 +378,60 @@ import { Subscription } from "../models/subscription.model.js";
             }
         ])
     })
+
+    const getWatchHistory= asyncHandler(async(req,res)=>{
+        const user= await User.aggregate([
+            {
+                $match:{
+                    _id:new mongoose.Types.ObjectId(req.user._id)
+                }
+            },
+            {
+                $lookup:{
+                    from:"videos",
+                    localField:"watchHistory",
+                    foreignField:"_id",
+                    as:"watchHistory",
+                    pipeline:[
+                        {
+                            $lookup:{
+                                from:"users",
+                                localField:"owner",
+                                foreignField:"_id",
+                                as:"owner",
+                                pipeline:[
+                                    {
+                                        $project:{
+                                            fullName:1,
+                                            username:1,
+                                            avatar:1
+                                        }
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            $addFields:{
+                                onwer:{
+                                    $first: "$owner"
+                                }
+                            }
+                        }
+                    ]
+                }     
+            }
+        ])
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                user[0].watchHistory,
+                "watch History Fetched successfully "
+            )
+        )
+    })
     export {
         registerUser,
         loginUser,
@@ -387,5 +442,6 @@ import { Subscription } from "../models/subscription.model.js";
         updateAccountDetails,
         updateAvatar,
         updateCoverImage,
-        getUserChannelProfile
+        getUserChannelProfile,
+        getWatchHistory
     }
